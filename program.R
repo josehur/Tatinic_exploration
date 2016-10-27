07229c4e840de4f0245a450f0df4bbf7155dd9f8
Jose Luis HURTADO <jose-luis.hurtadomedina@worldline.com>
2016-10-27 17:35:48 +0200

    commit 3

--- program.R
+++ program.R
@@ -4,6 +4,7 @@ library(ggplot2)
 library(ggthemes) # visualization
 library(scales) # visualization
 library(mice) #imputation
+library(randomForest) #imputation
 
 
 ############################################ Reading files
@@ -108,3 +109,102 @@ set.seed(129)
 # Perform mice imputation, excluding certain less-than-useful variables:
 mice_mod <- mice(total_data[, !names(total_data) %in% c('PassengerId','Name','Ticket','Cabin','Family','Surname','Survived'), with = F],
                  method='rf') 
+
+
+mice_mod$imp$Age
+
+# Save the complete output 
+mice_output <- complete(mice_mod)
+
+# Plot age distributions
+par(mfrow=c(1,2))
+hist(total_data$Age, freq=F, main='Age: Original Data', 
+     col='darkgreen', ylim=c(0,0.04))
+hist(mice_output$Age, freq=F, main='Age: MICE Output', 
+     col='lightgreen', ylim=c(0,0.04))
+
+
+# par(mfrow=c(1,2))
+# hist(total_data$Age, freq=T, main='Age: Original Data', 
+#      col='darkgreen')
+# hist(mice_output$Age, freq=T, main='Age: MICE Output', 
+#      col='lightgreen', )
+
+# Replace Age variable from the mice model.
+total_data$Age <- mice_output$Age
+
+# Show new number of missing Age values
+sum(is.na(total_data$Age))
+
+# First we'll look at the relationship between age & survival
+ggplot(total_data[1:891,], aes(Age, fill = factor(Survived))) + 
+  geom_histogram() + 
+  # I include Sex since we know (a priori) it's a significant predictor
+  facet_grid(.~Sex) + 
+  theme_few()
+
+# Create the column child, and indicate whether child or adult
+total_data$Child[total_data$Age < 18] <- 'Child'
+total_data$Child[total_data$Age >= 18] <- 'Adult'
+
+# Show counts
+table(total_data$Child, total_data$Survived)
+
+
+# Adding Mother variable
+total_data$Mother <- 'Not Mother'
+total_data$Mother[total_data$Sex == 'female' & total_data$Parch > 0 & total_data$Age > 18 & total_data$Title != 'Miss'] <- 'Mother'
+
+# Show counts
+table(total_data$Mother, total_data$Survived)
+
+
+# Finish by factorizing our two new factor variables
+total_data$Child  <- factor(total_data$Child)
+total_data$Mother <- factor(total_data$Mother)
+
+md.pattern(total_data)
+
+######################################## Prediction
+
+# Split the data back into a train set and a test set
+train <- total_data[1:891,]
+test <- total_data[892:1309,]
+
+
+set.seed(754)
+
+# Build the model (note: not all possible variables are used)
+rf_model <- randomForest(factor(Survived) ~ Pclass + Sex + Age + SibSp + Parch + 
+                           Fare + Embarked + Title + 
+                           Family_size_d + Child + Mother,
+                         data = train)
+
+# Show model error
+plot(rf_model, ylim=c(0,0.36))
+legend('topright', colnames(rf_model$err.rate), col=1:3, fill=1:3)
+
+# Get importance
+importance    <- importance(rf_model)
+varImportance <- data.frame(Variables = row.names(importance), 
+                            Importance = round(importance[ ,'MeanDecreaseGini'],2))
+
+# Create a rank variable based on importance
+rankImportance <- varImportance %>%
+  mutate(Rank = paste0('#',dense_rank(desc(Importance))))
+
+# Use ggplot2 to visualize the relative importance of variables
+ggplot(rankImportance, aes(x = reorder(Variables, Importance), 
+                           y = Importance, fill = Importance)) +
+  geom_bar(stat='identity') + 
+  geom_text(aes(x = Variables, y = 0.5, label = Rank),
+            hjust=0, vjust=0.55, size = 4, colour = 'red') +
+  labs(x = 'Variables') +
+  coord_flip() + 
+  theme_few()
+
+# Predict using the test set
+prediction <- predict(rf_model, test)
+
+# Save the solution to a dataframe with two columns: PassengerId and Survived (prediction)
+solution <- data.frame(PassengerID = test$PassengerId, Survived = prediction)
